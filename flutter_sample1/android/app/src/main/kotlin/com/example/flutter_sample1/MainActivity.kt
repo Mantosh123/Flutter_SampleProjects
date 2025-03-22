577ff235d2214430ec9bf84abd5a00c475305c17
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-03-22 20:17:24 +0530

    Change to medical shop.

--- flutter_sample1/android/app/src/main/kotlin/com/example/flutter_sample1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_sample1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ my_medical/android/app/src/main/kotlin/com/example/my_medical/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_medical
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
